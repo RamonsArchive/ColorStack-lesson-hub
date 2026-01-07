c4c36e6f924c29c741017fcd1fe05825aa5056d2
RamonsArchive <rmcdeem.m@gmail.com>
2026-01-07 13:05:32 -0800

    fixed file structure for demo and finished

---
+++ finished/src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import InputOutput from "../components/InputOutput";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full font-sans bg-slate-950">
+      <div className="flex flex-col w-full gap-3 max-w-3xl mx-auto py-10 md:py-20 px-5 sm:px-20">
+        <div className="w-full max-w-3xl flex items-start justify-start">
+          <div className="flex flex-row gap-2 items-center">
+            <div className="relative w-10 h-10 aspect-square">
+              <Image
+                src="/icon.png"
+                alt="ColorStack Icon"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <h3 className="text-md font-light text-zinc-500 italic">
+              ColorStack @ UCSD
+            </h3>
+          </div>
+        </div>
+        <div className="flex flex-col w-full max-w-3xl">
+          <h1 className="text-4xl font-bold">Lesson 1: Github & CI/CD</h1>
+        </div>
+        <InputOutput />
+      </div>
+    </main>
+  );
+}
